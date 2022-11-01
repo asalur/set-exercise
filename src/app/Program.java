2dfa67838fedd0681660674e8a60f030069dea54
Pedro Ruiz Marrara <pedrormarrara@gmail.com>
2022-11-01 16:53:51 +0000

    treeSet comparable

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,24 +1,20 @@
 package app;
 
-import java.util.Arrays;
 import java.util.Set;
 import java.util.TreeSet;
+import entities.Product;
 public class Program {
+
     public static void main(String[] args) {
 
-        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
-        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
-//union
-        Set<Integer> c = new TreeSet<>(a);
-        c.addAll(b);
-        System.out.println(c);
-//intersection - Elementos em comum nos conjuntos.
-        Set<Integer> d = new TreeSet<>(a);
-        d.retainAll(b);
-        System.out.println(d);
-//difference - Elementos diferentes entre os conjuntos.
-        Set<Integer> e = new TreeSet<>(a);
-        e.removeAll(b);
-        System.out.println(e);
+        Set<Product> set = new TreeSet<>();
+
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+
+        for (Product p : set) {
+            System.out.println(p);
+        }
     }
 }
